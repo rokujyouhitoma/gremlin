fa78612a0ab677fb891cebbc0f29aaca7bb48142
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2020-12-24 16:33:18 +0900

    Setup

--- src/something/sample.py
+++ src/something/sample.py
@@ -9,14 +9,14 @@ Variable("label").assignment(globalContext.someOperation())
 Variable("label").assignment(g.addE("label"))
 """
 
-from dataclasses import dataclass, field
 import typing
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Command:
     type: str
-    arguments: list
+    arguments: typing.List[typing.Any]
 
 
 class Operator:
@@ -24,35 +24,32 @@ class Operator:
 
 
 class GraphTraversal(Operator):
-    """
-    see: http://tinkerpop.apache.org/javadocs/current/full/org/apache/tinkerpop/gremlin/process/traversal/dsl/graph/GraphTraversal.html
-    """
     commands: typing.List[Command] = [Command("g", [])]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<GraphTraversal:{str(self.commands)}>"
 
-    def addE(self, edgeLabel: str):
+    def addE(self, edgeLabel: str) -> "GraphTraversal":
         self.commands.append(Command("addE", [edgeLabel]))
         return self
 
-    def addV(self, vertexLabel: str = ""):
+    def addV(self, vertexLabel: str = "") -> "GraphTraversal":
         self.commands.append(Command("addV", [vertexLabel]))
         return self
 
-    def has(self, propertyKey: str, value: typing.Any):
+    def has(self, propertyKey: str, value: typing.Any) -> "GraphTraversal":
         self.commands.append(Command("has", [propertyKey, value]))
         return self
 
-    def out(self, edgeLabel: str):
+    def out(self, edgeLabel: str) -> "GraphTraversal":
         self.commands.append(Command("out", [edgeLabel]))
         return self
 
-    def V(self):
+    def V(self) -> "GraphTraversal":
         self.commands.append(Command("V", []))
         return self
 
-    def values(self, propertyKey: str):
+    def values(self, propertyKey: str) -> "GraphTraversal":
         self.commands.append(Command("values", [propertyKey]))
         return self
 
@@ -64,23 +61,24 @@ class Variable(Operator):
     def __init__(self, name: str):
         self.name = name
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<Variable:{str(self.commands)}>"
 
-    def assignment(self, g: GraphTraversal):
+    def assignment(self, g: GraphTraversal) -> "Variable":
         self.commands.append(Command("variable", [self.name, g]))
         return self
 
 
 @dataclass
 class Context(Operator):
-    commands: typing.List[Command] = field(default_factory=list)
+    operators: typing.List[Operator] = field(default_factory=list)
 
 
 g = GraphTraversal()
-context = Context([
-    Variable("x").assignment(g.V().has("name","gremlin").out("knows").out("knows").values("name"))
-])
-
-import pprint
-pprint.pprint(context)
+context = Context(
+    [
+        Variable("x").assignment(
+            g.V().has("name", "gremlin").out("knows").out("knows").values("name")
+        )
+    ]
+)
