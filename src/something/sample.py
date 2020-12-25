9224f967928c0f2b815d90dd60702c9eeba3b167
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2020-12-25 15:26:59 +0900

    Refactoring

--- src/something/sample.py
+++ src/something/sample.py
@@ -11,58 +11,57 @@ from something.nodes import (
 
 
 class GraphTraversal(Node):
-    operators: typing.List[Node] = [gNode()]
+    nodes: typing.List[Node] = [gNode()]
 
     def generate(self) -> typing.Generator[Node, None, None]:
-        for operator in self.operators:
-            yield operator
+        for node in self.nodes:
+            yield node
 
     def evaluate(self) -> str:
-        return "".join(operator.evaluate() for operator in self.generate())
+        return "".join(node.evaluate() for node in self.generate())
 
     def addE(self, edgeLabel: str) -> "GraphTraversal":
-        self.operators.append(FunctionNode("addE", [edgeLabel]))
+        self.nodes.append(FunctionNode("addE", [edgeLabel]))
         return self
 
     def addV(self, vertexLabel: str = "") -> "GraphTraversal":
-        self.operators.append(FunctionNode("addV", [vertexLabel]))
+        self.nodes.append(FunctionNode("addV", [vertexLabel]))
         return self
 
     def has(self, propertyKey: str, value: typing.Any) -> "GraphTraversal":
-        self.operators.append(FunctionNode("has", [propertyKey, value]))
+        self.nodes.append(FunctionNode("has", [propertyKey, value]))
         return self
 
     def out(self, edgeLabel: str) -> "GraphTraversal":
-        self.operators.append(FunctionNode("out", [edgeLabel]))
+        self.nodes.append(FunctionNode("out", [edgeLabel]))
         return self
 
     def V(self) -> "GraphTraversal":
-        self.operators.append(FunctionNode("V", []))
+        self.nodes.append(FunctionNode("V", []))
         return self
 
     def values(self, propertyKey: str) -> "GraphTraversal":
-        self.operators.append(FunctionNode("values", [propertyKey]))
+        self.nodes.append(FunctionNode("values", [propertyKey]))
         return self
 
 
 class Variable(Node):
     name: str
-    operators: typing.List[Node] = []
+    nodes: typing.List[Node] = []
 
     def __init__(self, name: str):
         self.name = name
 
     def generate(self) -> typing.Generator[Node, None, None]:
-        for operator in self.operators:
-            yield operator
+        for node in self.nodes:
+            yield node
 
     def evaluate(self) -> str:
-        return "".join(operator.evaluate() for operator in (self.generate()))
+        return "".join(node.evaluate() for node in (self.generate()))
 
     def assignment(self, g: GraphTraversal) -> "Variable":
-        # TODO:
-        self.operators.append(IdentifierNode(self.name))
-        self.operators.append(EqualNode())
-        self.operators.append(g)
-        self.operators.append(SemicolonNode())
+        self.nodes.append(IdentifierNode(self.name))
+        self.nodes.append(EqualNode())
+        self.nodes.append(g)
+        self.nodes.append(SemicolonNode())
         return self
