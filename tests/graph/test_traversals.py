9425f1fd1a2f8df3d9782f8375182359f304890d
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-03-15 07:25:33 +0900

    Refactoring test cases

--- tests/graph/test_traversals.py
+++ tests/graph/test_traversals.py
@@ -179,6 +179,8 @@ class TestDefaultGraphTraversal:
             ("fold", [], "g.V().fold()"),
             ("hasId", ["arg"], 'g.V().hasId("arg")'),
             ("hasId", ["arg1", "arg2"], 'g.V().hasId("arg1","arg2")'),
+            ("hasKey", ["arg"], 'g.V().hasKey("arg")'),
+            ("hasKey", ["arg1", "arg2"], 'g.V().hasKey("arg1","arg2")'),
         ],
     )
     def test_V_methods(
@@ -223,22 +225,6 @@ class TestDefaultGraphTraversal:
         g = DefaultGraphTraversal()
         assert g
         node = getattr(g.V(), method)(g.V())
-        assert node
-        assert node.evaluate() == expected
-
-    @pytest.mark.parametrize(
-        "test_args,expected",
-        [
-            (["arg"], 'g.V().hasKey("arg")'),
-            (["arg1", "arg2"], 'g.V().hasKey("arg1","arg2")'),
-        ],
-    )
-    def test_hasKey(self, test_args: typing.List[str], expected: str) -> None:
-        from gremlin.graph import DefaultGraphTraversal
-
-        g = DefaultGraphTraversal()
-        assert g
-        node = g.V().hasKey(*test_args)
         assert node
         assert node.evaluate() == expected
 
