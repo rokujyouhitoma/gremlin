239955ddcdf9c6ba9529a46318b4fc28a5b44068
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-04-12 20:14:23 +0900

    Refactoring test case

--- tests/graph/test_traversals.py
+++ tests/graph/test_traversals.py
@@ -220,6 +220,7 @@ class TestDefaultGraphTraversal:
             ("outE", ["arg1", "arg2"], 'g.V().outE("arg1","arg2")'),
             ("outV", [], "g.V().outV()"),
             ("peerPressure", [], "g.V().peerPressure()"),
+            ("profile", [], "g.V().profile()"),
         ],
     )
     def test_V_methods(
@@ -269,21 +270,6 @@ class TestDefaultGraphTraversal:
         g = DefaultGraphTraversal()
         assert g
         node = getattr(g.V(), method)(g.V())
-        assert node
-        assert node.evaluate() == expected
-
-    @pytest.mark.parametrize(
-        "test_args,expected",
-        [
-            ([], "g.V().profile()"),
-        ],
-    )
-    def test_profile(self, test_args: typing.List[str], expected: str) -> None:
-        from gremlin.graph import DefaultGraphTraversal
-
-        g = DefaultGraphTraversal()
-        assert g
-        node = g.V().profile(*test_args)
         assert node
         assert node.evaluate() == expected
 
