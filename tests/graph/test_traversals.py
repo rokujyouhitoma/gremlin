52b8b5661f8596a95b37279c07c38f2c71e1f51d
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-05-01 09:51:00 +0900

    Refactoring test case

--- tests/graph/test_traversals.py
+++ tests/graph/test_traversals.py
@@ -246,6 +246,7 @@ class TestDefaultGraphTraversal:
             ("valueMap", [], "g.V().valueMap()"),
             ("values", ["arg"], 'g.V().values("arg")'),
             ("with_", ["arg"], 'g.V().with("arg")'),
+            ("write", [], "g.V().write()"),
         ],
     )
     def test_V_methods(
@@ -295,21 +296,6 @@ class TestDefaultGraphTraversal:
         g = DefaultGraphTraversal()
         assert g
         node = getattr(g.V(), method)(g.V())
-        assert node
-        assert node.evaluate() == expected
-
-    @pytest.mark.parametrize(
-        "test_args,expected",
-        [
-            ([], "g.V().write()"),
-        ],
-    )
-    def test_write(self, test_args: typing.List[str], expected: str) -> None:
-        from gremlin.graph import DefaultGraphTraversal
-
-        g = DefaultGraphTraversal()
-        assert g
-        node = g.V().write(*test_args)
         assert node
         assert node.evaluate() == expected
 
